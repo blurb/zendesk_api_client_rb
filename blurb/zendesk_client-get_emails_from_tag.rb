fc61df52860e04ab9aa1d4f5cd9bfb1e256d8903
Rishi Paranjape <rparanjape@blurb.com>
2016-03-01 16:54:42 -0800

    blurb zendesk api client

---
+++ blurb/zendesk_client-get_emails_from_tag.rb
@@ -0,0 +1,32 @@
+require 'zendesk_api'
+require 'logger'
+
+if ARGV[0].nil? || ARGV[1].nil? || ARGV[2].nil?
+  logger = Logger.new(STDOUT)
+  logger.info("Usage: ruby zendesk_client-get_emails_from_tag.rb 'blurb_zendesk_email' 'password' 'zendesk_tag'")
+  abort
+end
+
+client = ZendeskAPI::Client.new do |config|
+  # Mandatory:
+  config.url = "https://blurb.zendesk.com/api/v2"
+
+  # Basic / Token Authentication
+  config.username = ARGV[0]
+  config.password = ARGV[1]
+
+  # Retry uses middleware to notify the user
+  # when hitting the rate limit, sleep automatically,
+  # then retry the request.
+  config.retry = true
+end
+
+def process_item(item)
+  logger = Logger.new(STDOUT) 
+  logger.info "#{item.result_type},#{item.id},#{item.via.source.from.address}"
+end
+
+search_results = client.search(:query => "type:ticket tags:" + ARGV[2])
+
+search_results.all(1) {|item| process_item(item)}
+
